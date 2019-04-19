e51d910a571a78087691c81c292134aa484eb525
Tony Tia <webmaster@tonytia.com>
2019-04-19 00:27:38 -0700

    add server

--- app.py
+++ app.py
@@ -10,6 +10,7 @@ df = pd.read_csv('bees_final.csv')
 external_stylesheets = ['https://codepen.io/chriddyp/pen/bWLwgP.css']
 
 app = dash.Dash(__name__, external_stylesheets=external_stylesheets)
+server = app.server
 
 app.layout = html.Div([
     html.Div([
